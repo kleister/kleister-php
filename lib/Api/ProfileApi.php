305a6dbb928021d8dd0e2751e9abda44cc6f821c
tboerger <156964+tboerger@users.noreply.github.com>
2024-06-13 08:19:48 +0000

    feat: automated openapi client update

--- lib/Api/ProfileApi.php
+++ lib/Api/ProfileApi.php
@@ -1373,6 +1373,24 @@ class ProfileApi
             }
         }
 
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('Cookie');
+        if ($apiKey !== null) {
+            $headers['Cookie'] = $apiKey;
+        }
+        // this endpoint requires HTTP basic authentication
+        if (!empty($this->config->getUsername()) || !(empty($this->config->getPassword()))) {
+            $headers['Authorization'] = 'Basic ' . base64_encode($this->config->getUsername() . ":" . $this->config->getPassword());
+        }
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('X-API-Key');
+        if ($apiKey !== null) {
+            $headers['X-API-Key'] = $apiKey;
+        }
+        // this endpoint requires Bearer authentication (access token)
+        if (!empty($this->config->getAccessToken())) {
+            $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
+        }
 
         $defaultHeaders = [];
         if ($this->config->getUserAgent()) {
